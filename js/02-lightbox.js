3d9acc1a68f2cbb784505655fce769efb14bda94
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-06-09 22:24:02 +0300

    update2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,9 +10,9 @@ const imagesMarkup = createImegaesCardsMarkup(galleryItems);
 function createImegaesCardsMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
+      return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+</a></li>`;
     })
     .join('');
 }
